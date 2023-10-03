11a26303e3903fd832d274406f0a62927f82a879
Idunno1 <thegamemaster210@gmail.com>
2023-10-03 03:49:11 -0500

    fixed being able to move while opening the dimensional boxes

--- libraries/magical-glass/scripts/world/cutscenes/cell.lua
+++ libraries/magical-glass/scripts/world/cutscenes/cell.lua
@@ -1,14 +1,12 @@
 return {
     box_a = function(cutscene)
         Assets.stopAndPlaySound("dimbox")
-        Game.world.timer:after(5/30, function() -- todo: this doesn't lock your movement????
-            Game.world:openMenu(LightStorageMenu("items", "box_a"))
-        end)
+        cutscene:wait(5/30)
+        cutscene:after(function() Game.world:openMenu(LightStorageMenu("items", "box_a")) end)
     end,
     box_b = function(cutscene)
         Assets.stopAndPlaySound("dimbox")
-        Game.world.timer:after(5/30, function()
-            Game.world:openMenu(LightStorageMenu("items", "box_b"))
-        end)
+        cutscene:wait(5/30)
+        cutscene:after(function() Game.world:openMenu(LightStorageMenu("items", "box_b")) end)
     end
 }
--- mod.lua
+++ mod.lua
@@ -6,7 +6,6 @@ function Mod:postInit()
     Game:setFlag("#hide_cell", false)
     Game:setFlag("has_cell_phone", true)
     Game:setFlag("#savename_lw_menus", true)
-
 end
 
 function Mod:load()
