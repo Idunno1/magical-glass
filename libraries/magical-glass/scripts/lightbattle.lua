07d33c63a014f01765b0454d3a9de586c6e4122f
FireRainV <youaredied1234@gmail.com>
2023-12-03 18:33:55 +0200

    Usage of the setTension function in the hotkey

--- libraries/magical-glass/scripts/lightbattle.lua
+++ libraries/magical-glass/scripts/lightbattle.lua
@@ -2587,9 +2587,6 @@ function LightBattle:onKeyPressed(key)
         if key == "n" then
             NOCLIP = not NOCLIP
         end
-        if key == "n" then
-            NOCLIP = not NOCLIP
-        end
         if key == "delete" then
             for _,party in ipairs(self.party) do
                 party.chara:setHealth(999)
