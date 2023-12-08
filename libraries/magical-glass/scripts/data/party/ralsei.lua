c4bb3264365feef8067423db8418933244a4e0fb
FireRainV <youaredied1234@gmail.com>
2023-12-08 22:22:17 +0200

    Bug free! (for real) [hopefully]

--- libraries/magical-glass/scripts/data/party/ralsei.lua
+++ libraries/magical-glass/scripts/data/party/ralsei.lua
@@ -1,12 +1,12 @@
-local character, super = Class("kris", true)
+local character, super = Class("ralsei", true)
 
 function character:init()
     super.init(self)
     
-    self.weapon = "wood_blade"
+    self.weapon = "red_scarf"
     if Game.chapter >= 2 then
         self.armor[1] = "amber_card"
-        self.armor[2] = "amber_card"
+        self.armor[2] = "white_ribbon"
     end
 end
 
