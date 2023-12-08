d98f143978078668eb3056a568b1db6cc35a0b0f
FireRainV <youaredied1234@gmail.com>
2023-12-09 00:02:14 +0200

    Tityness

--- mod.lua
+++ mod.lua
@@ -6,7 +6,11 @@ function Mod:postInit()
     Game:setFlag("has_cell_phone", true)
 end
 
-function Mod:load()
+function Mod:load(new_file)
+    if new_file then
+        Game.money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
+        Game.lw_money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
+    end
     Game.world:registerCall("Dimensional Box A", "cell.box_a", false, 5/30)
     Game.world:registerCall("Dimensional Box B", "cell.box_b", false, 5/30)
 end
