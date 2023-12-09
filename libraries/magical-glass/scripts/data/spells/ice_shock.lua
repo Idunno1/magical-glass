55d8512e0517650e799a1d4e95f22908a268a3be
FireRainV <youaredied1234@gmail.com>
2023-12-09 18:54:28 +0200

    Nerfed Ice Shock

--- libraries/magical-glass/scripts/data/spells/ice_shock.lua
+++ libraries/magical-glass/scripts/data/spells/ice_shock.lua
@@ -7,7 +7,7 @@ end
 function spell:getDamage(user, target)
     if Game:isLight() then
         local min_magic = Utils.clamp(user.chara:getStat("magic") - 2, 1, 999)
-        return math.ceil((min_magic * 15) + 30 + Utils.random(10))
+        return math.ceil((min_magic * 15) + 20 + Utils.random(5))
     else
         local min_magic = Utils.clamp(user.chara:getStat("magic") - 10, 1, 999)
         return math.ceil((min_magic * 30) + 90 + Utils.random(10))
