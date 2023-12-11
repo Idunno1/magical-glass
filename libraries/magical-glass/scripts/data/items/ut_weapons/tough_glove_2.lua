f577e215b0f9f2c837c0cf684331a773c8e0d5fc
FireRainV <youaredied1234@gmail.com>
2023-12-11 22:31:17 +0200

    Changed the function to trigger the correct one

--- libraries/magical-glass/scripts/data/items/ut_weapons/tough_glove_2.lua
+++ libraries/magical-glass/scripts/data/items/ut_weapons/tough_glove_2.lua
@@ -91,7 +91,7 @@ function item:onLightAttack(battler, enemy, damage, stretch, crit)
         end
         enemy:hurt(damage, battler)
 
-        battler.chara:onAttackHit(enemy, damage)
+        battler.chara:onLightAttackHit(enemy, damage)
         this:remove()
 
         Game.battle:endAttack()
