2a363d39134ff3276d0001e99c3f433de03372a3
FireRainV <youaredied1234@gmail.com>
2024-01-04 22:32:09 +0200

    same thing for the light battle

--- libraries/magical-glass/scripts/lightbattle/lightenemybattler.lua
+++ libraries/magical-glass/scripts/lightbattle/lightenemybattler.lua
@@ -770,7 +770,11 @@ function LightEnemyBattler:freeze()
 
     Game.battle.timer:tween(20/30, sprite, {freeze_progress = 1})
 
-    Game.battle.money = Game.battle.money + 2
+    if Game:isLight() then
+        Game.battle.money = Game.battle.money + 2
+    else
+        Game.battle.money = Game.battle.money + 24
+    end
     self:defeat("FROZEN", true)
 end
 
