68dbe929219e7d78e6ff1e48dd307d5ae827060c
DiamondBor <99621601+DiamondBor@users.noreply.github.com>
2024-01-07 21:08:06 -0300

    YOU CANNOT USE 0.8.1!!!

--- libraries/magical-glass/lib.lua
+++ libraries/magical-glass/lib.lua
@@ -105,6 +105,10 @@ function lib:load(data, new_file)
 end
 
 function lib:preInit()
+
+    if Kristal.Version < SemVer("v0.9.0-dev") then
+        error("You must use Kristal 0.9.0-dev or a newer version to run Magical Glass.")
+    end
     
     self.random_encounters = {}
     self.light_encounters = {}
