2179bb2d874aa37b188d2134b38e47bafc1a514d
FireRainV <youaredied1234@gmail.com>
2024-01-27 10:46:49 +0200

    third

--- mod.lua
+++ mod.lua
@@ -4,8 +4,8 @@ end
 
 function Mod:load(new_file)
     if new_file then
-        Game.money = Game:getFlag("debug", true) and 1000 or 0
-        Game.lw_money = Game:getFlag("debug", true) and 1000 or 0
+        Game.money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
+        Game.lw_money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
     end
     Game.world:registerCall("Dimensional Box A", "cell.box_a", false, 5/30)
     Game.world:registerCall("Dimensional Box B", "cell.box_b", false, 5/30)
