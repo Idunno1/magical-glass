6836c51337d04e05540cfd03e7d9c1ef1742b372
FireRainV <youaredied1234@gmail.com>
2024-01-29 14:24:31 +0200

    damage spells will now use super.getDamage
    This behaves similarly to orig(self)

--- libraries/magical-glass/scripts/data/spells/ice_shock.lua
+++ libraries/magical-glass/scripts/data/spells/ice_shock.lua
@@ -9,8 +9,7 @@ function spell:getDamage(user, target)
         local min_magic = Utils.clamp(user.chara:getStat("magic") - 3, 1, 999)
         return math.ceil((min_magic * 8) + 45 + Utils.random(10))
     else
-        local min_magic = Utils.clamp(user.chara:getStat("magic") - 10, 1, 999)
-        return math.ceil((min_magic * 30) + 90 + Utils.random(10))
+        return super.getDamage(self, user, target)
     end
 end
 
