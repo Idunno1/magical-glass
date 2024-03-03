e849e4d7a0434fb7b085cf8a81c0cdae6a1e27ec
FireRainV <youaredied1234@gmail.com>
2024-03-04 00:04:39 +0200

    even more improved

--- libraries/magical-glass/scripts/lightshop.lua
+++ libraries/magical-glass/scripts/lightshop.lua
@@ -595,15 +595,18 @@ function LightShop:draw()
                     
                     table.insert(stats_diff, difference .. " ")
                 end
+                love.graphics.print(current_item.item:getLightTypeName(), left + 28, top + 28)
                 if #Game.party == 2 then
-                    love.graphics.print({current_item.item:getLightTypeName() .. "\n(",{Game.party[1]:getLightColor()},stats_diff[1],{Game.party[2]:getLightColor()},stats_diff[2],{1,1,1},stat ..")\n" .. current_item.options["description"]}, left + 28, top + 28)
+                    love.graphics.print({"(",{Game.party[1]:getLightColor()},stats_diff[1],{Game.party[2]:getLightColor()},stats_diff[2],{1,1,1},stat ..")"}, left + 28, top + 28 * 2)
                 elseif #Game.party == 3 then
-                    love.graphics.print({current_item.item:getLightTypeName() .. "\n(",{Game.party[1]:getLightColor()},stats_diff[1],{Game.party[2]:getLightColor()},stats_diff[2],{Game.party[3]:getLightColor()},stats_diff[3],{1,1,1},stat ..")\n" .. current_item.options["description"]}, left + 28, top + 28)
+                    love.graphics.print({"(",{Game.party[1]:getLightColor()},stats_diff[1],{Game.party[2]:getLightColor()},stats_diff[2],{Game.party[3]:getLightColor()},stats_diff[3],{1,1,1},stat ..")"}, left + 28, top + 28 * 2)
                 else
-                    love.graphics.print({current_item.item:getLightTypeName() .. "\n(",{1,1,1},stats_diff[1],{1,1,1},stat ..")\n" .. current_item.options["description"]}, left + 28, top + 28)
+                    love.graphics.print("(" .. stats_diff[1] .. stat ..")", left + 28, top + 28 * 2)
                 end
+                love.graphics.print(current_item.options["description"], left + 28, top + 28 * 3)
             else
-                love.graphics.print(current_item.item:getLightTypeName() .. "\n" .. current_item.options["description"], left + 28, top + 28)
+                love.graphics.print(current_item.item:getLightTypeName(), left + 28, top + 28)
+                love.graphics.print(current_item.options["description"], left + 28, top + 28 * 2)
             end
 
             Draw.popScissor()
