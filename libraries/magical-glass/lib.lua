4f788c27dd534f402d4b2d4987acee9823e7400f
FireRainV <youaredied1234@gmail.com>
2024-03-10 19:47:55 +0200

    Revert "will only affect light worlds"
    This reverts commit e84fe5b178dab19844948d7e8bbb50bcbb70acb7.

--- libraries/magical-glass/lib.lua
+++ libraries/magical-glass/lib.lua
@@ -1859,112 +1859,111 @@ function lib:init()
 
     if not Mod.libs["widescreen"] then
         Utils.hook(WorldCutscene, "text", function(orig, self, text, portrait, actor, options)
+            local function waitForTextbox(self) return not self.textbox or self.textbox:isDone() end
+            if type(actor) == "table" and not isClass(actor) then
+                options = actor
+                actor = nil
+            end
+            if type(portrait) == "table" then
+                options = portrait
+                portrait = nil
+            end
+        
+            options = options or {}
+        
+            self:closeText()
+        
+            local width, height = 529, 103
             if Game:isLight() then
-                local function waitForTextbox(self) return not self.textbox or self.textbox:isDone() end
-                if type(actor) == "table" and not isClass(actor) then
-                    options = actor
-                    actor = nil
-                end
-                if type(portrait) == "table" then
-                    options = portrait
-                    portrait = nil
-                end
-            
-                options = options or {}
-            
-                self:closeText()
-            
                 width, height = 530, 104
-            
-                self.textbox = Textbox(56, 344, width, height)
-                self.textbox.text.hold_skip = false
-                self.textbox.layer = WORLD_LAYERS["textbox"]
-                Game.world:addChild(self.textbox)
-                self.textbox:setParallax(0, 0)
-            
-                local speaker = self.textbox_speaker
-                if not speaker and isClass(actor) and actor:includes(Character) then
-                    speaker = actor.sprite
+            end
+        
+            self.textbox = Textbox(56, 344, width, height)
+            self.textbox.text.hold_skip = false
+            self.textbox.layer = WORLD_LAYERS["textbox"]
+            Game.world:addChild(self.textbox)
+            self.textbox:setParallax(0, 0)
+        
+            local speaker = self.textbox_speaker
+            if not speaker and isClass(actor) and actor:includes(Character) then
+                speaker = actor.sprite
+            end
+        
+            if options["talk"] ~= false then
+                self.textbox.text.talk_sprite = speaker
+            end
+        
+            actor = actor or self.textbox_actor
+            if isClass(actor) and actor:includes(Character) then
+                actor = actor.actor
+            end
+            if actor then
+                self.textbox:setActor(actor)
+            end
+        
+            if options["top"] == nil and self.textbox_top == nil then
+                local _, player_y = Game.world.player:localToScreenPos()
+                options["top"] = player_y > 260
+            end
+            if options["top"] or (options["top"] == nil and self.textbox_top) then
+            local bx, by = self.textbox:getBorder()
+            self.textbox.y = by + 2
+            end
+        
+            self.textbox.active = true
+            self.textbox.visible = true
+            self.textbox:setFace(portrait, options["x"], options["y"])
+        
+            if options["reactions"] then
+                for id,react in pairs(options["reactions"]) do
+                    self.textbox:addReaction(id, react[1], react[2], react[3], react[4], react[5])
                 end
-            
-                if options["talk"] ~= false then
-                    self.textbox.text.talk_sprite = speaker
+            end
+        
+            if options["functions"] then
+                for id,func in pairs(options["functions"]) do
+                    self.textbox:addFunction(id, func)
                 end
-            
-                actor = actor or self.textbox_actor
-                if isClass(actor) and actor:includes(Character) then
-                    actor = actor.actor
-                end
-                if actor then
-                    self.textbox:setActor(actor)
-                end
-            
-                if options["top"] == nil and self.textbox_top == nil then
-                    local _, player_y = Game.world.player:localToScreenPos()
-                    options["top"] = player_y > 260
-                end
-                if options["top"] or (options["top"] == nil and self.textbox_top) then
-                local bx, by = self.textbox:getBorder()
-                self.textbox.y = by + 2
-                end
-            
-                self.textbox.active = true
-                self.textbox.visible = true
-                self.textbox:setFace(portrait, options["x"], options["y"])
-            
-                if options["reactions"] then
-                    for id,react in pairs(options["reactions"]) do
-                        self.textbox:addReaction(id, react[1], react[2], react[3], react[4], react[5])
-                    end
-                end
-            
-                if options["functions"] then
-                    for id,func in pairs(options["functions"]) do
-                        self.textbox:addFunction(id, func)
-                    end
-                end
-            
-                if options["font"] then
-                    if type(options["font"]) == "table" then
-                        -- {font, size}
-                        self.textbox:setFont(options["font"][1], options["font"][2])
-                    else
-                        self.textbox:setFont(options["font"])
-                    end
-                end
-            
-                if options["align"] then
-                    self.textbox:setAlign(options["align"])
-                end
-            
-                self.textbox:setSkippable(options["skip"] or options["skip"] == nil)
-                self.textbox:setAdvance(options["advance"] or options["advance"] == nil)
-                self.textbox:setAuto(options["auto"])
-            
-                if false then -- future feature
-                    self.textbox:setText("[wait:2]"..text, function()
-                        self.textbox:remove()
-                        self:tryResume()
-                    end)
+            end
+        
+            if options["font"] then
+                if type(options["font"]) == "table" then
+                    -- {font, size}
+                    self.textbox:setFont(options["font"][1], options["font"][2])
                 else
-                    self.textbox:setText(text, function()
-                        self.textbox:remove()
-                        self:tryResume()
-                    end)
+                    self.textbox:setFont(options["font"])
                 end
-            
-                local wait = options["wait"] or options["wait"] == nil
-                if not self.textbox.text.can_advance then
-                    wait = options["wait"] -- By default, don't wait if the textbox can't advance
-                end
-            
-                if wait then
-                    return self:wait(waitForTextbox)
-                else
-                    return waitForTextbox, self.textbox
-                end
+            end
+        
+            if options["align"] then
+                self.textbox:setAlign(options["align"])
+            end
+        
+            self.textbox:setSkippable(options["skip"] or options["skip"] == nil)
+            self.textbox:setAdvance(options["advance"] or options["advance"] == nil)
+            self.textbox:setAuto(options["auto"])
+        
+            if false then -- future feature
+                self.textbox:setText("[wait:2]"..text, function()
+                    self.textbox:remove()
+                    self:tryResume()
+                end)
             else
-                orig(self, text, portrait, actor, options)
+                self.textbox:setText(text, function()
+                    self.textbox:remove()
+                    self:tryResume()
+                end)
+            end
+        
+            local wait = options["wait"] or options["wait"] == nil
+            if not self.textbox.text.can_advance then
+                wait = options["wait"] -- By default, don't wait if the textbox can't advance
+            end
+        
+            if wait then
+                return self:wait(waitForTextbox)
+            else
+                return waitForTextbox, self.textbox
             end
         end)
     end
